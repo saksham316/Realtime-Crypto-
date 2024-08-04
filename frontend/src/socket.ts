b5cbf06d7c2776e8670f58f0e18bc2e804156535
saksham <phantomdx316@gmail.com>
2024-08-05 01:23:16 +0530

    added the env frontend

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 const URL =
-  process.env.REACT_ENV === "production" ? "" : "http://localhost:6990";
+  process.env.REACT_ENV === "production" ? "" : `${process.env.REACT_APP_NODE_APP_DEV_URL}`;
 
 export const socket = io(URL);
